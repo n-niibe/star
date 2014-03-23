94e0dafadfb80191f437a2fa723bff562095c7be
Nozomu Niibe <nozomu.niibe@pro-spire.co.jp>
2014-03-23 13:18:34 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Star::Application.config.secret_key_base = 'b8fd58783b02041e39106cc7612fd13b2be822498b7eb220d759a165172ab0c06af2460d5d41e0c0a3dcfdf71827dbc6372780816e9414fd4870acdab2d5f068'
