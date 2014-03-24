5b5c076ee156a5aed46845572fba43392a507894
Nozomu Niibe <nozomu.niibe@pro-spire.co.jp>
2014-03-24 15:03:21 +0000

    TDD

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Star::Application.config.secret_key_base = 'b8fd58783b02041e39106cc7612fd13b2be822498b7eb220d759a165172ab0c06af2460d5d41e0c0a3dcfdf71827dbc6372780816e9414fd4870acdab2d5f068'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Star::Application.config.secret_key_base = secure_token
